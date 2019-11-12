88e4961530a11a1625ab3920f20f3a28ac09cb4f
Christian Luis <christianlmc1@gmail.com>
2019-11-12 20:53:05 -0300

    Validação de linha e de coluna

--- main.hs
+++ main.hs
@@ -1,12 +1,26 @@
+import Data.List
+
 printBoard :: [[Int]] -> IO ()
 printBoard element = mapM_ print (element)
+
+validRow :: Int -> [Int] -> Bool
+validRow x line = not (x `elem` line)
+
+validColumn :: Int -> [Int] -> Bool
+validColumn x column = not (x `elem` column)
+
+isValid :: Int -> Int -> Int -> [[Int]] -> Bool
+isValid value row col sudoku =
+    let rowIsValid = validRow value (sudoku !! row) 
+        colIsValid = validColumn value (transpose sudoku !! col)
+    in (rowIsValid && colIsValid)
 
 main = do
     -- Aqui os valores com 0 são os espaços vazios
     let sudoku = [
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0],
+                [0,0,0,3,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
@@ -14,8 +28,9 @@ main = do
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0]
             ]    
-        
-    printBoard sudoku
+
+    print (isValid 3 2 0 sudoku)
+    printBoard (sudoku)
     
 
 
