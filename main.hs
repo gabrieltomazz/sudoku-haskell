85ed7e4966087357f3117b59875c16c922fea9e7
Christian Luis <christianlmc1@gmail.com>
2019-11-12 23:58:34 -0300

    Adicionando validacao do quadrado 3x3

--- main.hs
+++ main.hs
@@ -1,7 +1,7 @@
 import Data.List
 
 printBoard :: [[Int]] -> IO ()
-printBoard element = mapM_ print (element)
+printBoard sudoku = mapM_ print (sudoku)
 
 validRow :: Int -> [Int] -> Bool
 validRow x line = not (x `elem` line)
@@ -9,18 +9,25 @@ validRow x line = not (x `elem` line)
 validColumn :: Int -> [Int] -> Bool
 validColumn x column = not (x `elem` column)
 
+validSquare :: Int -> Int -> Int -> [[Int]] -> Bool
+validSquare value row col sudoku =
+    let row_square = [(row `div` 3 * 3)..(row `div` 3 * 3)+2]
+        col_square = [(col `div` 3 * 3)..(col `div` 3 * 3)+2]
+    in not (value `elem` [ sudoku !! y !! x | y <- row_square, x <- col_square])
+
 isValid :: Int -> Int -> Int -> [[Int]] -> Bool
 isValid value row col sudoku =
     let rowIsValid = validRow value (sudoku !! row) 
         colIsValid = validColumn value (transpose sudoku !! col)
-    in (rowIsValid && colIsValid)
+        squareIsValid = validSquare value row col sudoku
+    in (rowIsValid && colIsValid && squareIsValid)
 
 main = do
     -- Aqui os valores com 0 são os espaços vazios
     let sudoku = [
-                [0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0],
-                [0,0,0,3,0,0,0,0,0],
+                [0,0,0,1,4,0,0,0,0],
+                [0,0,0,2,5,0,0,0,0],
+                [0,0,0,3,6,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0],
@@ -28,9 +35,9 @@ main = do
                 [0,0,0,0,0,0,0,0,0],
                 [0,0,0,0,0,0,0,0,0]
             ]    
-
-    print (isValid 3 2 0 sudoku)
-    printBoard (sudoku)
+    print (isValid 5 0 3 sudoku)
+    -- print (isValid 3 2 0 sudoku)
+    -- printBoard (sudoku)
     
 
 
